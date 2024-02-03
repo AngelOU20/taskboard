d2301ea2ff4bf07dcc8498a1ba2d583fb8f440ec
Julio Ucharima <julio_ucharima@usmp.pe>
2024-02-03 13:05:32 -0500

    feat: add textarea support to useEditing hook

--- hooks/useEditing.ts
+++ hooks/useEditing.ts
@@ -2,10 +2,12 @@ import { ElementRef, RefObject, useRef, useState } from "react";
 
 interface EditingOptions {
   isEditing: boolean,
-  enableEditing: () => void,
+  enableInputEditing: () => void,
+  enableTextareaEditing: () => void,
   disableEditing: () => void,
   formRef: RefObject<HTMLFormElement>,
   inputRef: RefObject<HTMLInputElement>,
+  textareaRef: RefObject<HTMLTextAreaElement>;
 }
 
 export const useEditing = (): EditingOptions => {
@@ -14,11 +16,20 @@ export const useEditing = (): EditingOptions => {
   const formRef = useRef<ElementRef<'form'>>(null);
   const inputRef = useRef<ElementRef<'input'>>(null);
 
-  const enableEditing = () => {
+  const textareaRef = useRef<ElementRef<"textarea">>(null);
+
+  const enableInputEditing = () => {
     setIsEditing(true);
     setTimeout(() => {
       inputRef.current?.focus();
       inputRef.current?.select();
+    });
+  };
+
+  const enableTextareaEditing = () => {
+    setIsEditing(true);
+    setTimeout(() => {
+      textareaRef.current?.focus();
     });
   };
 
@@ -28,9 +39,11 @@ export const useEditing = (): EditingOptions => {
 
   return {
     isEditing,
-    enableEditing,
+    enableInputEditing,
+    enableTextareaEditing,
     disableEditing,
     formRef,
     inputRef,
+    textareaRef,
   };
 };
