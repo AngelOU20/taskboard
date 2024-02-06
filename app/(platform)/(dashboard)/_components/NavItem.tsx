69f6a2843e979f8ba18e3d099298f31ff2a5b129
Julio Ucharima <julio_ucharima@usmp.pe>
2024-02-06 11:48:09 -0500

    feat: add NavItem component to Navbar component

---
+++ app/(platform)/(dashboard)/_components/NavItem.tsx
@@ -0,0 +1,111 @@
+'use client';
+
+import Image from 'next/image';
+import {
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from '@/components/ui/accordion';
+import { Activity, CreditCard, Layout, Settings } from 'lucide-react';
+import { cn } from '@/lib/utils';
+import { useRouter, usePathname } from 'next/navigation';
+import { Button } from '@/components/ui/button';
+import { Skeleton } from '@/components/ui/skeleton';
+
+export type Organization = {
+  id: string;
+  slug: string;
+  imageUrl: string;
+  name: string;
+};
+
+interface NavItemProps {
+  isActive: boolean;
+  isExpanded: boolean;
+  organization: Organization;
+  onExpand: (id: string) => void;
+}
+
+export const NavItem = ({
+  isActive,
+  isExpanded,
+  organization,
+  onExpand,
+}: NavItemProps) => {
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const routes = [
+    {
+      label: 'Boards',
+      icon: <Layout className="h-4 w-4 mr-2" />,
+      href: `/organization/${organization.id}`,
+    },
+    {
+      label: 'Activity',
+      icon: <Activity className="h-4 w-4 mr-2" />,
+      href: `/organization/${organization.id}/activity`,
+    },
+    {
+      label: 'Settings',
+      icon: <Settings className="h-4 w-4 mr-2" />,
+      href: `/organization/${organization.id}/settings`,
+    },
+  ];
+
+  const onClick = (href: string) => {
+    router.push(href);
+  };
+
+  return (
+    <AccordionItem value={organization.id} className="border-none">
+      <AccordionTrigger
+        onClick={() => onExpand(organization.id)}
+        className={cn(
+          'flex items-center gap-x-2 p-1.5 text-neutral-700 rounded-md transition text-start hover:bg-neutral-500/10 hover:no-underline',
+          isActive && !isExpanded && 'bg-fuchsia-500/10 text-fuchsia-700'
+        )}
+      >
+        <div className="flex items-center gap-x-2">
+          <div className="w-7 h-7 relative">
+            <Image
+              fill
+              src={organization.imageUrl}
+              alt="Organization"
+              className="rounded-sm object-cover"
+            />
+          </div>
+          <span className="font-medium text-sm">{organization.name}</span>
+        </div>
+      </AccordionTrigger>
+      <AccordionContent className="pt-1 text-neutral-700">
+        {routes.map((route) => (
+          <Button
+            key={route.label}
+            size="sm"
+            variant="ghost"
+            onClick={() => onClick(route.href)}
+            className={cn(
+              'w-full font-normal justify-start pl-10 mb-1',
+              pathname === route.href && 'bg-fuchsia-500/10 text-fuchsia-700'
+            )}
+          >
+            {route.icon}
+            {route.label}
+          </Button>
+        ))}
+      </AccordionContent>
+    </AccordionItem>
+  );
+};
+
+NavItem.Skeleton = function SkeletonNavItem() {
+  return (
+    <div className="flex items-center gap-x-2">
+      <div className="w-10 h-10 relative shrink-0">
+        <Skeleton className="h-full w-full absolute" />
+      </div>
+      <Skeleton className="h-10 w-full" />
+    </div>
+  );
+};
